29fda07a555918cf42d242476e4722f65c90a269
kozhevin <d.kozhevin@rian.ru>
2020-02-03 10:30:13 +0300

    Init

---
+++ app/src/main/java/ru/rian/testanhuawei/MyPushService.java
@@ -0,0 +1,42 @@
+package ru.rian.testanhuawei;
+
+import android.util.Log;
+
+import com.huawei.hms.push.HmsMessageService;
+import com.huawei.hms.push.RemoteMessage;
+
+public class MyPushService extends HmsMessageService {
+
+    public static final String TAG = "PushDemoLog";
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.i(TAG, "receive token:" + token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.i(TAG, "onMessageReceived is called");
+        if (remoteMessage == null) {
+            Log.e(TAG, "Received message entity is null!");
+            return;
+        }
+
+        if (remoteMessage.getData().length() > 0) {
+            Log.i(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+        if (remoteMessage.getNotification() != null) {
+            Log.i(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+    @Override
+    public void onMessageSent(String s) {
+    }
+
+    @Override
+    public void onSendError(String s, Exception e) {
+    }
+
+}
